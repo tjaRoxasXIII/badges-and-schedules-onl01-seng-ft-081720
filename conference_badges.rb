89621a46c8b69d97cb7caab4fd69cd24e9392aae
Taylor Allen <tjaroguetech@gmail.com>
2020-08-18 16:52:41 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -26,6 +26,6 @@ def assign_rooms(attendees)
 end
 
 def printer(attendees)
-  i = 0
-  puts batch_badge_creator(attendees[i])
+  puts batch_badge_creator(attendees)
+  puts assign_rooms(attendees)
 end 
