322fad19a66ac1e6b879ff05d748405a8ce842aa
Taylor Allen <tjaroguetech@gmail.com>
2020-08-18 16:01:56 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,19 @@
 # Write your code here.
+def badge_maker(name)
+ return "Hello, my name is #{name}."
+end 
+
+def batch_badge_creator(attendees)
+  attendees.each do |badge_name|
+   return "Hello, my name is #{badge_name}."
+  end
+end
+
+def assign_rooms(attendees)
+  room = 1 
+  attendees.each do |room_assignments|
+    room_assignments = "Hello, #{attendees}! You'll be assigned to room #{room}!"
+    room += 1
+  end
+  return room_assignments
+end
