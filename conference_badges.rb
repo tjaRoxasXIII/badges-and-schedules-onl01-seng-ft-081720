5cde3b4e75ef5b1ad7682f0ac6222c112afc65b2
Taylor Allen <tjaroguetech@gmail.com>
2020-08-18 16:39:22 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -14,9 +14,10 @@ def batch_badge_creator(attendees)
 end
 
 def assign_rooms(attendees)
+  i = 0 
   room = 1 
   attendees.each do |room_assignments|
-    room_assignments = "Hello, #{attendees}! You'll be assigned to room #{room}!"
+    room_assignments[i] = "Hello, #{attendees}! You'll be assigned to room #{room}!"
     room += 1
   end
   return room_assignments
